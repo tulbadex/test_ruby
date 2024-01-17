fe1433d9a0d6df53b7013d4f70132e565b7422f8
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2024-01-17 03:48:13 -0800

    added flash message

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
     def index
-        
+        flash[:notice] = "Successfully created..."
+        flash[:alert] = "Invalid email or password"
     end
 end
